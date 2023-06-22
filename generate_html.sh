00acad5556a1dfa26a13e47e711c0f1808bc0f2e
Pascal Seitz <pascal.seitz@gmail.com>
2023-06-22 12:40:08 +0800

    update literate docs

--- generate_html.sh
+++ generate_html.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/build"
-echo $DIR
-rm -fr $DIR
-mkdir -p $DIR
-cd $DIR
-npm install docco@0.8
-rm -fr tantivy
-git clone git@github.com:tantivy-search/tantivy.git
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/build"
+#echo $DIR
+#rm -fr $DIR
+#mkdir -p $DIR
+#cd $DIR
+npm install docco
+#rm -fr tantivy
+git clone git@github.com:tantivy-search/tantivy.git --depth=1
 #git clone /home/paul/git/tantivy
-cd tantivy/examples
-for example in $(ls *.rs)
-do
-    docco $example -o "../../html"
-done
-cd ${DIR}
-rm -fr ../examples
-cp -R html ../examples
+cd tantivy/examples || exit
+docco *.rs -o ../../examples/
+cd - || exit
+#cd ${DIR}
+#rm -fr ../examples
+#cp -R html ../examples
 
 # building the mdbook
 #cd tantivy/doc
@@ -24,3 +22,5 @@ cp -R html ../examples
 #cd ${DIR}
 #rm -fr ../book
 #cp -R tantivy/doc/book ../book
+
+rm -fr tantivy
