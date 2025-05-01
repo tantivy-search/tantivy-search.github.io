5a6525bd31dedf6fd75425d4bff802e1a67d1496
Adrien Grand <jpountz@gmail.com>
2025-05-01 21:38:28 +0200

    Use built app that can be served from any path.
    This applies the commit from
    https://github.com/quickwit-oss/search-benchmark-game/pull/67 that configures
    `homepage: .` in `package.json` so that the same site can be served from `/`
    (e.g. when running `make serve` in `search-benchmark-game`) or from `/bench`
    (public results of the benchmark).

---
+++ bench/precache-manifest.4877fb426cff0e5e3e85119ae25212cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d9c8537bec3d391dd70b46253b6849",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e693c92dc5ab46e35720",
+    "url": "./static/css/main.27669ca4.chunk.css"
+  },
+  {
+    "revision": "c551a61abf2385a8f0bc",
+    "url": "./static/js/2.b9a6fc97.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "./static/js/2.b9a6fc97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e693c92dc5ab46e35720",
+    "url": "./static/js/main.d83a9b45.chunk.js"
+  },
+  {
+    "revision": "56dc2eb95cdb5052533e",
+    "url": "./static/js/runtime-main.6b2c0276.js"
+  }
+]);
